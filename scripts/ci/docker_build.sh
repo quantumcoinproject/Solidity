3c18da1352f5415440c804832a53288311f17a66
Blake Hartin <bh@bh>
2024-02-03 00:55:43 +0000

    Add support for build ubuntu and windows

---
+++ scripts/ci/docker_build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+function error() {
+  echo >&2 "ERROR: ${1} Aborting." && false
+}
+
+function warning() {
+  echo >&2 "WARNING: ${1}"
+}
+
+#[[ $# == 3 ]] || error "Expected exactly 3 parameters: '${0} <IMAGE_NAME> <IMAGE_VARIANT> <DOCKER_REPOSITORY>'."
+
+IMAGE_NAME="${1}"
+IMAGE_VARIANT="${2}"
+DOCKER_REPOSITORY="${3}"
+CONTAINER_NAME="${4}"
+DOCKERFILE="scripts/docker/${IMAGE_NAME}/Dockerfile.${IMAGE_VARIANT}"
+
+#echo "-- check_dockerfile_was_changed"
+
+# exit, if the dockerfile was not changed.
+#if git diff --quiet origin/develop HEAD -- "${DOCKERFILE}"; then
+#  echo "${DOCKERFILE} was not changed. Nothing to do."
+#  exit 0
+#fi
+
+
+echo "-- build_docker"
+
+# This is a workaround: we run `docker build` twice to prevent the `layer does not exist` problem.
+# See https://github.com/moby/moby/issues/37965.
+docker build "scripts/docker/${IMAGE_NAME}" --file "scripts/docker/${IMAGE_NAME}/Dockerfile.${IMAGE_VARIANT}" --tag "${IMAGE_NAME}" ||
+  docker build "scripts/docker/${IMAGE_NAME}" --file "scripts/docker/${IMAGE_NAME}/Dockerfile.${IMAGE_VARIANT}" --tag "${IMAGE_NAME}"
