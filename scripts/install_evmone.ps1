0beafdf841e3dffd73d99b7be6e024d535245891
Quantum Coin <contact@quantumcoin.org>
2024-11-29 18:36:59 -0800

    Update install_evmone.ps1 mkdir -p

--- scripts/install_evmone.ps1
+++ scripts/install_evmone.ps1
@@ -5,5 +5,5 @@ $progressPreference = "silentlyContinue"
 
 Invoke-WebRequest -URI "https://github.com/ethereum/evmone/releases/download/v0.5.0/evmone-0.5.0-windows-amd64.zip" -OutFile "evmone.zip"
 tar -xf evmone.zip "bin/evmone.dll"
-mkdir deps
+mkdir -p deps
 mv bin/evmone.dll deps
