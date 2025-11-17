9bf7b04d723b90c39f2fd15fc33c7d853d74b364
blakehartin <bh@bh>
2025-11-16 21:39:46 -0800

    optimize compiler help and prerelease.txt fix

--- libsolidity/interface/CompilerStack.cpp
+++ libsolidity/interface/CompilerStack.cpp
@@ -1239,7 +1239,7 @@ void CompilerStack::compileContract(
 				+ " bytes and exceeds 24576 bytes (a limit introduced in Spurious Dragon). "
 				  "This contract may not be deployable on Mainnet. "
 				  "Consider enabling the optimizer (with a low \"runs\" value!), "
-				  "turning off revert strings, or using libraries.");
+				  "turning off revert strings, or using libraries. Use --optimize or --optimize-runs for solc");
 
 	_otherCompilers[compiledContract.contract] = compiler;
 }
